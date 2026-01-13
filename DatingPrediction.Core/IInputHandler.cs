c316c43324f4a9862a82c3fe7aaeb1c1cd4fd299
amiruldevkun <amirulpanda123456@gmail.com>
2026-01-13 17:21:38 +0800

    Added displayString() method to the interface

--- DatingPrediction.Core/IInputHandler.cs
+++ DatingPrediction.Core/IInputHandler.cs
@@ -6,4 +6,5 @@ public interface IInputHandler
         int getNumericalInput();
         char getKeyboardPress();
         void displayQuestions(string input, string randoText);
+        string displayString(string input);
 }
